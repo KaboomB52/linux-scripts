ca24df6a65d146d326b7929c3d6ca929678d851c
Kaboom <60456400+KaboomB52@users.noreply.github.com>
2022-02-14 08:46:20 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install nano
+sudo apt-get install curl
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
+sudo apt update
+sudo apt-get install msopenjdk-11
+sudo apt-get install msopenjdk-8
+iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+iptables -A INPUT -s yourip -j ACCEPT
+iptables -A INPUT -p tcp --dport 25565 -j ACCEPT
+iptables -A INPUT -p tcp --dport 21 -j ACCEPT
+iptables -A INPUT -p tcp --dport 27017 -j DROP
+iptables -A INPUT -p tcp --dport 6379 -j DROP
+iptables -A INPUT -p tcp -j DROP
+iptables-save
+sudo apt-get install redis-server
+sudo apt-get install mongodb-server
+sudo apt-get install jenkins
+read -t 15 -N 1 -p "Do you want to setup jenkins? (y/N)? " answer
+echo 
+if [ "${answer,,}" == "y" ]
+then
+    sudo systemctl start jenkins
+    echo Open http://ci.mythicalmc.org/ and use this key,
+    sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+fi
