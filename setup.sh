071ad583b7e02945faed426e5d545d226c0f12b2
Ian <60456400+KaboomB52@users.noreply.github.com>
2022-03-04 07:46:26 -0600

    move both repos to one place

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sudo apt-get install nano
 sudo apt-get install curl
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo apt-add-repository https://packages.microsoft.com/ubuntu/20.04/prod
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt-get install msopenjdk-11
 iptables -A INPUT -s 127.0.0.1 -j ACCEPT
@@ -16,8 +18,6 @@ iptables -A INPUT -p tcp -j DROP
 iptables-save
 sudo apt-get install redis-server
 sudo apt-get install mongodb-server
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get install jenkins
 read -t 15 -N 1 -p "Do you want to setup jenkins? (y/N)? " answer
 echo 
